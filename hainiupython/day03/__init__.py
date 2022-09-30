5a6ff8d9b188cff9cb4def7707ada61b2d864c12
panhub10 <253724363@qq.com>
2022-09-30 17:50:24 +0800

    首次提交工程

---
+++ hainiupython/day03/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+__init__.py.py
+Created on 2022/9/30 17:48
+Copyright (c) 2022/9/30, 海牛学院版权所有.
+@author: 潘牛
+"""
---
+++ hainiupython/day03/m1.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+"""
+m1.py
+Created on 2022/9/30 17:48
+Copyright (c) 2022/9/30, 海牛学院版权所有.
+@author: 潘牛
+"""
+a = 1
+b = 2
+c = a + b
+print(c)
